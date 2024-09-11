b3235c13238b7eb4496b2bc8bd174f11ce96ab5e
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-09-12 07:59:59 +0800

    change text color of active tab

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap p-[10px] my-[6px] mr-[6px] text-sm font-medium text-text-primary transition-all disabled:pointer-events-none disabled:opacity-50 relative data-[state=active]:bg-footer data-[state=active]:after:bg-text-foreground data-[state=active]:after:content-[''] data-[state=active]:after:h-[2px] data-[state=active]:after:block data-[state=active]:after:absolute data-[state=active]:after:bottom-[-3px] data-[state=active]:after:w-full",
+      "inline-flex items-center justify-center whitespace-nowrap p-[10px] my-[6px] mr-[6px] text-sm font-medium text-text-primary transition-all disabled:pointer-events-none disabled:opacity-50 relative data-[state=active]:bg-footer data-[state=active]:after:bg-text-foreground data-[state=active]:after:content-[''] data-[state=active]:after:h-[2px] data-[state=active]:after:block data-[state=active]:after:absolute data-[state=active]:after:bottom-[-3px] data-[state=active]:after:w-full data-[state=active]:text-text-foreground",
       className
     )}
     {...props}
