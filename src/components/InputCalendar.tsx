0b29e19b9cc34631c1e25b4a5b682fb1228c132c
skyc4ffe1ne <skyc4ff3ine@virgilio.it>
2024-09-12 20:45:25 +0200

    Added input calendar, and daypicker

--- src/components/InputCalendar.tsx
+++ src/components/InputCalendar.tsx
@@ -1,8 +1,22 @@
-export default function InputCalendar() {
-  return (
-    <div className="absolute top-16 left-0">
-      <h1> Hello</h1>
-      <input type="date" />
-    </div>
-  )
+"use client";
+import { useState } from "react";
+
+import { DayPicker } from "react-day-picker";
+
+export default function InputCalendar({ type }) {
+    const [selected, setSelected] = useState();
+
+    return (
+        <div
+            className={`absolute shadow-md bg-popover rounded-3xl top-[76px] ${type === "date" ? "left-1/2" : "left-full"} -translate-x-1/2  border py-4 px-8 min-w-[803px] cursor-auto flex justify-center`}
+        >
+            <DayPicker
+                className="pt-[1.125rem] pb-2.5 px-7"
+                mode="range"
+                numberOfMonths={2}
+                selected={selected}
+                onSelect={setSelected}
+            />
+        </div>
+    );
 }
--- src/components/InputNavbar.tsx
+++ src/components/InputNavbar.tsx
@@ -12,69 +12,73 @@ import { Search } from "lucide-react";
 import { Tab } from "./Header";
 
 interface Tabinput {
-  activeTab: Tab;
+    activeTab: Tab;
 }
 
 type nameModal = "" | "destination" | "checkin" | "checkout" | "who" | "date";
 
 export default function InputNavbar({ activeTab }: Tabinput) {
-  const [activeModals, setActiveModals] = useState<nameModal>("");
-  const containerModalRef = useActiveModals(setActiveModals);
+    const [activeModals, setActiveModals] = useState<nameModal>("");
+    const containerModalRef = useActiveModals(setActiveModals);
 
-  return (
-    <div className="w-full flex items-center justify-center border-b pb-4">
-      <div
-        className={`rounded-full shadow-3xl flex relative ${activeModals && "bg-[#ebebeb] z-10"}`}
-        ref={containerModalRef}
-      >
-        <Inputs type="big" name="destination" activeModals={activeModals}>
-          <p className="text-xs font-medium text-textNavbar-primary">Where</p>
-          <input
-            type="text"
-            placeholder="Search destination"
-            className="text-sm text-[hsl(0,0%,69%)] border-none outline-none focus:border-none focs:outline-none bg-transparent"
-          />
-          {activeModals === "destination" ? <InputModals /> : ""}
-        </Inputs>
-        {activeTab === "stays" ? (
-          <>
-            <Inputs type="small" name="checkin" activeModals={activeModals}>
-              <p className="text-xs font-medium text-textNavbar-primary">
-                Check in
-              </p>
-              <p className="text-sm text-textNavbar-secondary"> Add dates</p>
-              {activeModals === "checkin" ? <InputCalendar /> : ""}
-            </Inputs>
+    return (
+        <div className="w-full flex items-center justify-center border-b pb-4">
+            <div
+                className={`rounded-full shadow-3xl flex relative ${activeModals && "bg-[#ebebeb] z-10"}`}
+                ref={containerModalRef}
+            >
+                <Inputs type="big" name="destination" activeModals={activeModals}>
+                    <p className="text-xs font-medium text-textNavbar-primary">Where</p>
+                    <input
+                        type="text"
+                        placeholder="Search destination"
+                        className="text-sm text-[hsl(0,0%,69%)] border-none outline-none focus:border-none focs:outline-none bg-transparent"
+                    />
+                    {activeModals === "destination" ? <InputModals /> : ""}
+                </Inputs>
+                {activeTab === "stays" ? (
+                    <>
+                        <Inputs type="small" name="checkin" activeModals={activeModals}>
+                            <p className="text-xs font-medium text-textNavbar-primary">
+                                Check in
+                            </p>
+                            <p className="text-sm text-textNavbar-secondary"> Add dates</p>
+                            {activeModals === "checkin" ? <InputCalendar /> : ""}
+                        </Inputs>
 
-            <Inputs type="small" name="checkout" activeModals={activeModals}>
-              <p className="text-xs font-medium text-textNavbar-primary">
-                Check out
-              </p>
-              <p className="text-sm text-textNavbar-secondary"> Add dates</p>
-              {activeModals === "checkout" ? (
-                <p className="absolute top-0 left-0"> Placeholder </p>
-              ) : (
-                ""
-              )}
-            </Inputs>
-          </>
-        ) : (
-          <Inputs type="big" name="date" activeModals={activeModals}>
-            <p className="text-xs font-medium text-textNavbar-primary"> Date</p>
-            <p className="text-sm text-textNavbar-secondary"> Add dates</p>
-            {activeModals === "date" ? <InputGuest /> : ""}
-          </Inputs>
-        )}
+                        <Inputs type="small" name="checkout" activeModals={activeModals}>
+                            <p className="text-xs font-medium text-textNavbar-primary">
+                                Check out
+                            </p>
+                            <p className="text-sm text-textNavbar-secondary"> Add dates</p>
+                            {activeModals === "checkout" ? (
+                                <p className="absolute top-0 left-0"> Placeholder </p>
+                            ) : (
+                                ""
+                            )}
+                        </Inputs>
+                    </>
+                ) : (
+                    <Inputs type="big" name="date" activeModals={activeModals}>
+                        <p className="text-xs font-medium text-textNavbar-primary"> Date</p>
+                        <p className="text-sm text-textNavbar-secondary"> Add dates</p>
+                        {activeModals === "date" ? (
+                            <InputCalendar type={activeModals} />
+                        ) : (
+                            ""
+                        )}
+                    </Inputs>
+                )}
 
-        <Inputs type="bigNoBorder" name="who" activeModals={activeModals}>
-          <p className="text-xs font-medium text-textNavbar-primary"> Who</p>
-          <p className="text-sm text-textNavbar-secondary"> Add guests</p>
-          {activeModals === "who" ? <InputGuest /> : ""}
-        </Inputs>
-        <div className="absolute top-1/2 -translate-y-1/2 right-2 cursor-pointer w-12 h-12 bg-bgaccent hover:brightness-90 flex justify-center items-center rounded-full ">
-          <Search size={18} color="#ffffff" strokeWidth={3} />
+                <Inputs type="bigNoBorder" name="who" activeModals={activeModals}>
+                    <p className="text-xs font-medium text-textNavbar-primary"> Who</p>
+                    <p className="text-sm text-textNavbar-secondary"> Add guests</p>
+                    {activeModals === "who" ? <InputGuest /> : ""}
+                </Inputs>
+                <div className="absolute top-1/2 -translate-y-1/2 right-2 cursor-pointer w-12 h-12 bg-bgaccent hover:brightness-90 flex justify-center items-center rounded-full ">
+                    <Search size={18} color="#ffffff" strokeWidth={3} />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
