f0a878e9c5b370267e7571be6b500733caf4c055
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-09-17 15:27:32 +0800

    removed TestFooter from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Footer from "@/components/Footer";
-import TestFooter from "@/components/TestFooter";
 
 export default function Home() {
   return (
     <>
       <Footer />
-      <TestFooter />
     </>
   )
 } 
